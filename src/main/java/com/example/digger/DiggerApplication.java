6202ebaf9e23d1420f5f5861d0c47457f408800b
nam <yhnham@gmail.com>
2025-01-16 18:02:04 +0900

    first commit

---
+++ src/main/java/com/example/digger/DiggerApplication.java
@@ -0,0 +1,13 @@
+package com.example.digger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiggerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiggerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/digger/DiggerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.digger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiggerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
